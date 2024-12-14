1628fd0d211542ae0cc690595dd780f180c76da5
Ryqite <garbaruk.901@gmail.com>
2024-12-14 03:17:26 +0300

    first commit

---
+++ app/src/main/java/com/example/modsen/PresentationLayer/DetailPage.kt
@@ -0,0 +1,6 @@
+package com.example.modsen.PresentationLayer
+
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailPage : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/example/modsen/PresentationLayer/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.modsen.PresentationLayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.modsen.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
